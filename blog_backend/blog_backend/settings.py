3966c25e13928cdd85828f7fb6b1ca76e9736741
GMD369 <chdryghulam475@gmail.com>
2025-07-30 15:45:34 +0500

    updated settings.py

--- blog_backend/blog_backend/settings.py
+++ blog_backend/blog_backend/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.getenv('SECRET_KEY', 'django-insecure-#($qfob2sqmwsgkuc-j(htdv*^
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG', 'True').lower() == 'true'
 
-ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', 'localhost,127.0.0.1').split(',')
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', 'blog-web-app-qkjc.onrender.com,localhost,127.0.0.1').split(',')
 
 # Application definition
 
